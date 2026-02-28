6089b985bf32a4de2e8d01182621781f65e128f6
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-02-28 14:51:34 +0000

    WIP

--- src/app/pokemon/detail/detail.pokemon.ts
+++ src/app/pokemon/detail/detail.pokemon.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { POKEMONS } from '../../mock/mock_pokemon';
+import { Pokemons } from '../../pokemon';
+import { CustomtypeColorPipe } from '../../shared/pipes/type.color.pipe';
 @Component({
   selector: 'app-detail.pokemon',
-  imports: [],
+  imports: [CustomtypeColorPipe],
   templateUrl: './detail.pokemon.html',
   styleUrl: './detail.pokemon.css',
 })
 export class DetailPokemon implements OnInit {
   private route = inject(ActivatedRoute);
 
+  private pokemonList!: Pokemons[];
+  public pokemon!: Pokemons | undefined;
+
   ngOnInit(): void {
+    this.pokemonList = POKEMONS;
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
-    console.log('Snapshot', pokemonId);
+    const id = Number(pokemonId);
+    this.pokemon = this.pokemonList.find((pokemon) => pokemon.id === id);
+    console.table(this.pokemon);
   }
 }
